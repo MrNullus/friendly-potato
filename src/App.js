2197fa2f69355f8745f13f5819342ade4ee4342a
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-23 03:39:49 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
+
 import './styles/global';
 
 function App() {
   return (
-    <Register/>
+    <Home />
   );
 }
 
