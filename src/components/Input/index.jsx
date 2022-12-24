8eedb050423ea09cdc9a404e5689a622e1075cb9
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-23 22:40:09 -0300

    changes in input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 
+import './styles.scss';
+
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className="input">
+      <input 
+        type="text"
+        placeholder="Mande um byte..."
+        name=""
+        id=""
+      />
+
+      <div className="send">
+        <img 
+          src="" 
+          alt="" 
+        />
+
+        <input 
+          type="file" 
+          className="sr-only" 
+          name=""
+          id=""
+        />
+        <label htmlFor="">
+          <img 
+            src="" 
+            alt="" 
+          />
+        </label>
+        
+        <button>Enviar!</button>
+      </div>
+    </div>
   );
 };
 
