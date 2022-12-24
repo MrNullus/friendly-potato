47de5e39dbba0e11a5d7da4d4cce29610a835b25
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-23 22:39:23 -0300

    changes input

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import './styles.scss';
+
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message owner">
+      <div className="menssageInfo">
+        <img 
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/280dd4105648537.5f7dbf28b12f1.jpg"
+          alt="" 
+        />
+        <span>coisa boa</span>
+      </div>
+      <div className="menssageContent">
+        <p>hello from hellfire</p>
+        <img 
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/280dd4105648537.5f7dbf28b12f1.jpg"
+          alt="" 
+        />
+      </div>
+    </div>
   );
 };
 
