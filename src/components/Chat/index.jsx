5d80895b4e6d7a01ba53059d3017b42369eea5f2
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-23 22:39:05 -0300

    changes chat

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import ImgCam from '../../img/cam.png';
+import ImgAdd from '../../img/add.png';
+import ImgMore from '../../img/more.png';
+
+import Message from '../Message';
+import './styles.scss';
+
 const Chat = () => {
   return (
     <main className="chat">
-      Chat
+      <div className="chatInfo">
+        <span>Jane</span>
+
+        <div className="chatIcons">
+          <img src={ImgCam} alt="" />
+          <img src={ImgAdd} alt="Add" />
+          <img src={ImgMore} alt="More" />
+        </div>
+      </div>
+
+      <Message />
+      <Input />
     </main>
   );
 };
