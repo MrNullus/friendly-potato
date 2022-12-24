f09dc2b3b19dda0cb5fea646d7116f33a79fbba4
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-23 22:39:38 -0300

    changes in chats

--- src/components/Chats/index.jsx
+++ src/components/Chats/index.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className="chats">
+      <div className="userChat">
+        <img 
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/280dd4105648537.5f7dbf28b12f1.jpg"  
+          alt="" 
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello Hell</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img 
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/280dd4105648537.5f7dbf28b12f1.jpg"  
+          alt="" 
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello Hell</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
