12f0acbde7f867892a7db01128ffef59f5b33dbf
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-29 22:30:07 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContextProvider>
 );
