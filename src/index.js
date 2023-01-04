3577c4ec304c92217846be38dad3809fe3ab2293
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-03 23:09:29 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { AuthContextProvider } from './contexts/AuthContext';
+import { AuthContextProvider } from './context/AuthContext';
+import { ChatContextProvider } from './context/ChatContext';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
+    <ChatContextProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
